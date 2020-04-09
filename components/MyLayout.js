0a7c4c7c4e3c463caf59899828df5c6ca1f4a30d
sachin <sachinjangid021@gmail.com>
2020-04-10 01:42:55 +0530

    check sample github action

---
+++ components/MyLayout.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+};
+
+const withLayout = Page => {
+  return () => (
+      <div style={layoutStyle}>
+        <Header /> 
+        {Page.children}
+        <Page />
+      </div>
+  );
+};
+
+export default withLayout;
